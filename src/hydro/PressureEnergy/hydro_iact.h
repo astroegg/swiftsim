900323ebc578b783ef61ca7ede6a62256830ac21
loikki <loic.hausammann@protonmail.ch>
2019-12-19 10:23:41 +0100

    PU: change how the pressure floor is used

--- src/hydro/PressureEnergy/hydro_iact.h
+++ src/hydro/PressureEnergy/hydro_iact.h
@@ -262,9 +262,9 @@ __attribute__((always_inline)) INLINE static void runner_iact_force(
 
   /* Compute the ratio of pressures */
   const float pressure_inverse_i =
-      pi->force.pressure_bar_with_floor / (pi->pressure_bar * pi->pressure_bar);
+    1.f / pi->force.pressure_bar_with_floor;
   const float pressure_inverse_j =
-      pj->force.pressure_bar_with_floor / (pj->pressure_bar * pj->pressure_bar);
+    1.f / pj->force.pressure_bar_with_floor;
 
   /* SPH acceleration term */
   const float sph_acc_term = pj->u * pi->u * hydro_gamma_minus_one *
